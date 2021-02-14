32dfccb52c5a9c47d7a82064e6667f0cb304f99e
Tanmay-Sankpal <tanmaysankpal119>
2021-02-14 13:38:32 +0530

    Hello World

--- Basic Programs/hello.s
+++ Basic Programs/hello.s
@@ -1,1 +1,16 @@
-	.file	"hello.c"
+.section .rodata
+	msg:
+	.string "hello"
+.globl main
+main:
+
+#prolouge
+pushl %ebp
+movl %esp,%ebp
+
+pushl $msg 
+call printf
+addl $8,%esp
+
+pushl $0
+call exit
